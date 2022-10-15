b2d5f2a55cd1065a3058f3290eb7288c70009106
Maximo Cattaneo <cattaneomaxi@gmail.com>
2022-10-15 17:34:38 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,44 @@
+const Pizzas = [
+    {
+        id: 1,
+        nombre: "Fugazzeta",
+        ingredientes: ["Salsa de tomate", "Queso", "Cebolla"],
+        precio: 550,
+    },
+    {
+        id: 2,
+        nombre: "Napolitana",
+        ingredientes: ["Salsa de tomate", "Queso", "Rodajas de tomate", "Orégano", "Ajo"],
+        precio: 500,
+    },
+    {
+        id: 3,
+        nombre: "Especial",
+        ingredientes: ["Salsa de tomate", "Jamón", "Queso", "Orégano", "Huevo", "Pimientos"],
+        precio: 500,
+    },
+    {
+        id: 4,
+        nombre: "Jamón y queso",
+        ingredientes: ["Salsa de tomate", "Orégano", "Jamón", "Queso"],
+        precio: 450,
+    },
+    {
+        id: 5,
+        nombre: "Cantimpalo",
+        ingredientes: ["Salsa de tomate", "Cantimpalo", "Queso", "Orégano", "Aceitunas"],
+        precio: 550,
+    },
+    {
+        id: 6,
+        nombre: "Rúcula",
+        ingredientes: ["Salsa de tomate", "Queso", "Rúcula", "Jamón crudo"],
+        precio: 650,
+    },
+    {
+        id: 7,
+        nombre: "Panceta",
+        ingredientes: ["Queso", "Orégano", "Panceta", "Aceitunas"],
+        precio: 700,
+    },
+];
