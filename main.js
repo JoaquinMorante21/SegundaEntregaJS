d5d5841489330b2b781f17fb535771a11b76191d
Maximo Cattaneo <cattaneomaxi@gmail.com>
2022-10-20 17:15:24 -0300

    Trabajo Terminado

--- main.js
+++ main.js
@@ -47,27 +47,21 @@ const Pizzas = [
 const form = document.getElementById('form');
 const input = document.getElementById('input');
 const list = document.getElementById('list');
-
-// Traer elementos del LS si existen
-let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-// Grabar en LS
-const saveLocalStorage = (taskList) => {
-  localStorage.setItem('tasks', JSON.stringify(taskList))
-};
+let tasks = [];
 
 //Crear el elemento a renderizar en caso que el ID sea entre 1 y 7
 const createTask = task =>
 `
     <li class="card blue">
-        <h2>${task.nombre}</h2>
+        <h2>🍕${task.nombre}</h2>
         <h3>$${task.precio}</h3>
     </li>
 `;
 //Crear el elemento a renderizar en caso que el ID no este entre 1 y 7
-const wrongTask = () =>
+const wrongId = (pizza) =>
 `
     <li class="card red">
-        <h2>El id ingresado no coincide con ninguna pizza</h2>
+        <h2>El Id ${pizza.id} no coincide con ninguna pizza</h2>
     </li>
 `;
 //Crear el elemento a renderizar en caso que el usuario no escriba un numero
@@ -78,23 +72,33 @@ const noNumber = () =>
     </li>
 `;
 
-// Funcion que decide si el numero dado pertenece a un id
-const decider = (n) =>{
-    if (n < 1 || n > 7){
-        return wrongTask (n);
-    }
-    else if (n == null){
+// Funcion que decide si el objeto dado, pertenece a uno de los de la lista o no
+const decider = (pizza) =>{
+   /*  console.log(pizza); */
+    if (pizza == undefined){
         return noNumber ();
     }
+    else if (pizza.id < 0 || pizza.id > 7){
+        return wrongId (pizza);
+    }
     else{
-        return createTask (n);
+        return createTask (pizza);
     }
 };
 
 // Renderizar la o las tareas y corre la funcion que decide a que grupo pertenece el ID
 renderTask = TodoList => {
-    list.innerHTML = TodoList.map(task => decider(task)).join('');
-    console.log(list.innerHTML)
+    list.innerHTML = TodoList.map(pizza => decider(pizza)).join('');
+};
+
+//Funcion que detecta si el ID no esta entre el 1 y el 7, crea un objeto pizza con ese ID para usarlo en el mensaje
+const nuevoObjeto = (n)=>{
+    if (Number(n) < 0 || Number(n) > 7){
+        return Pizzas.push({id: Number(n) ,nombre:'n',});
+    }
+    else {
+        return;
+    }
 };
 
 
@@ -103,12 +107,13 @@ const init = () => {
         e.preventDefault();
         const task = input.value;
         input.value = '';
+        //Detectamos si el numero que puso el usuario es uno del 1 al 7 y si no lo es creamos un objeto
+        nuevoObjeto (task);
+
         // Filtra que pizza es la que le pasaron el numero de id
         const thisPizza = Pizzas.filter ((Pizza) => Pizza.id == task);
-        tasks = [...tasks, task];
-        saveLocalStorage(tasks);
-        renderTask(thisPizza);
+        tasks = [...tasks, thisPizza[0]];
+        renderTask(tasks);
     })
-    document.addEventListener('DomContentLoaded', renderTask(tasks));
 }
 init();
